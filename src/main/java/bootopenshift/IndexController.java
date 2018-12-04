e05593562c0949a6787a03032b3692bb3bc84be3
ricardodaza <37436223+ricardodaza@users.noreply.github.com>
2018-12-04 08:48:28 +0100

    import @pathvariable in controller

--- src/main/java/bootopenshift/IndexController.java
+++ src/main/java/bootopenshift/IndexController.java
@@ -2,6 +2,7 @@ package bootopenshift;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
